b239d1f12964f3d466e07534cae7f9955f85994b
tushar-2658 <36257197+tushar-2658@users.noreply.github.com>
2020-03-02 00:01:32 +0600

    Update sparse_table.cpp

--- sparse_table.cpp
+++ sparse_table.cpp
@@ -8,14 +8,14 @@ void build(vector<int>& v){
   for(int i = 0; i < n; i++){
     sparse[i][0] = v[i];
   }
-  for(int j = 1; j < 15; j++){
+  for(int j = 1; j < 19; j++){
     for(int i = 0; i < n; i++){
       sparse[i][j] = max(sparse[i][j - 1], sparse[i + (1 << (j - 1))][j - 1]);
     }
   }
 }
 
-int query(int id, int l, int r){
+int query(int l, int r){
   int j = log2(r - l + 1);
   return max(sparse[l][j], sparse[r - (1 << j) + 1][j]);
 }
